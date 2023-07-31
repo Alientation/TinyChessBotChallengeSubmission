60702bfed92163e99619ca2642d91eb1700c6380
Bill <billma.texas@gmail.com>
2023-07-31 16:27:13 -0500

    buttons wider

--- Chess-Challenge/src/Framework/Application/UI/MenuUI.cs
+++ Chess-Challenge/src/Framework/Application/UI/MenuUI.cs
@@ -9,8 +9,8 @@ namespace ChessChallenge.Application
     {
         public static void DrawButtons(ChallengeController controller)
         {
-            Vector2 buttonPos = UIHelper.Scale(new Vector2(260, 210));
-            Vector2 buttonSize = UIHelper.Scale(new Vector2(260, 55));
+            Vector2 buttonPos = UIHelper.Scale(new Vector2(220, 210));
+            Vector2 buttonSize = UIHelper.Scale(new Vector2(400, 55));
             float spacing = buttonSize.Y * 1.2f;
             float breakSpacing = spacing * 0.6f;
 
