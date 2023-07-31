ceedcf5dd809baad1fd4af17f7bff70696384724
Bill <billma.texas@gmail.com>
2023-07-31 11:02:54 -0500

    Debugging stats plus piece eval optimizations

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -8,12 +8,55 @@ using System.Collections.Generic;
     Features
     Min Max Algorithm
     Alpha Beta Pruning
+*/
+/*  STATS = {
+        1st Move Metric
+        Thinking time: 692 resulting in a board eval of 0 + (0)
+        Time for ThinkTime: 692 (692)
+        Time for MinimaxTime: 673 (33.65)
+        Time for evalBoardTime: 682 (0.05)
+        Time for evalPieceTime: 624 (0)
+        Time for getPossibleMovesTime: 0 (0)
+        Total moves: 15942
+        Total count for ThinkCount: 1
+        Total count for minimaxCount: 15962
+        Total count for minimaxCacheCount: 0
+        Total count for minimaxDepthCacheCount: 0
+        Total count for getPossibleMovesCount: 1618
+        Total count for getPossibleMovesCacheCount: 66
+        Total count for evalBoardCount: 14347
+        Total count for evalBoardCacheCount: 1508
+        Total count for evalPieceCount: 410448
+        Total count for evalPieceCacheCount: 410272
 
-    STATS = {
+        2nd Move Metric
 
-    }
 
-    
+
+        5th Move Metric
+
+
+
+        10th Move Metric
+
+
+
+        20th Move Metric
+
+
+
+
+        30th Move Metric
+
+
+        40th Move Metric
+
+
+        50th Move Metric
+
+    }*/
+
+    /*
     TODO
 
     Score board based off piece locations
@@ -242,8 +285,8 @@ public class MyBot : IChessBot
     Dictionary<ulong,int> minimaxCache = new Dictionary<ulong, int>();
 
 
-    int defaultSearchDepth = 4;
-    int autoMoveThreshold = 8;
+    int defaultSearchDepth = 3;
+    int autoMoveThreshold = 16;
 
     ///parses compressed piece values
     public MyBot() {
@@ -254,7 +297,7 @@ public class MyBot : IChessBot
             new List<int>(),
             new List<int>(),
         };
-        countLogs = new int[9];
+        countLogs = new int[10];
         moveLog = new List<Move>();
     }
 
@@ -305,7 +348,7 @@ public class MyBot : IChessBot
     /// algorithm to find best move
     public int minimax(Board board, int depth, float alpha, float beta, bool isMax, int maxDepth, int bestPrevEval) {
         /*if (minimaxCache.ContainsKey(board.ZobristKey)) {
-            logCount(LogCountType.minimaxSavedDepthCount, maxDepth - depth);
+            logCount(LogCountType.minimaxDepthCacheCount, maxDepth - depth);
             return minimaxCache[board.ZobristKey];
         } */
         logCount(LogCountType.minimaxCount, 1);
@@ -318,7 +361,7 @@ public class MyBot : IChessBot
                 board.MakeMove(move);
                 int eval;
                 /*if (minimaxCache.ContainsKey(board.ZobristKey)) {
-                    logCount(LogCountType.minimaxSavedCount, 1);
+                    logCount(LogCountType.minimaxCacheCount, 1);
                     eval = minimaxCache[board.ZobristKey];
                 } else*/ 
                     eval = minimax(board, depth+1, alpha, beta, false, maxDepth, bestPrevEval);
@@ -331,7 +374,7 @@ public class MyBot : IChessBot
                 if (beta <= alpha) break;
                 //if (maxEval < 0 && maxEval < bestPrevEval) return -10000;
             }
-            minimaxCache[board.ZobristKey] = maxEval;
+            //minimaxCache[board.ZobristKey] = maxEval;
             return maxEval;
         } else {
             int minEval = int.MaxValue;
@@ -339,7 +382,7 @@ public class MyBot : IChessBot
                 board.MakeMove(move);
                 int eval;
                 /*if (minimaxCache.ContainsKey(board.ZobristKey)) {
-                    logCount(LogCountType.minimaxSavedCount, 1);
+                    logCount(LogCountType.minimaxCacheCount, 1);
                     eval = minimaxCache[board.ZobristKey];
                 } else*/
                     eval = minimax(board, depth+1, alpha, beta, true, maxDepth, bestPrevEval);
@@ -352,7 +395,7 @@ public class MyBot : IChessBot
                 beta = Math.Min(beta, eval);
                 if (beta <= alpha) break;
             }
-            minimaxCache[board.ZobristKey] = minEval;
+            //minimaxCache[board.ZobristKey] = minEval;
             return minEval;
         }
     }
@@ -363,14 +406,17 @@ public class MyBot : IChessBot
         logCount(LogCountType.getPossibleMovesCount, 1);
         int timeGetMovesStart = cTimer.MillisecondsElapsedThisTurn;
 
-        if (movesFromBoardCache.ContainsKey(board.ZobristKey)) {
-            logCount(LogCountType.getPossibleMovesSavedHashCount, 1);
-            logTime(LogTimeType.getPossibleMovesTime, cTimer.MillisecondsElapsedThisTurn-timeGetMovesStart);
-            return movesFromBoardCache[board.ZobristKey];
-        }
-        Move[] moves = board.GetLegalMoves();
+        Move[] moves;
 
-        movesFromBoardCache.Add(board.ZobristKey, moves);
+        if (movesFromBoardCache.ContainsKey(board.ZobristKey)) {
+            logCount(LogCountType.getPossibleMovesCacheCount, 1);
+            moves = movesFromBoardCache[board.ZobristKey];
+        } else {
+            moves = board.GetLegalMoves();
+
+
+            movesFromBoardCache[board.ZobristKey] = moves;
+        }
 
         logTime(LogTimeType.getPossibleMovesTime, cTimer.MillisecondsElapsedThisTurn-timeGetMovesStart);
         return moves;
@@ -382,47 +428,47 @@ public class MyBot : IChessBot
         logCount(LogCountType.evalBoardCount, 1);
         int timeEvalBoardStart = cTimer.MillisecondsElapsedThisTurn;
 
-        if (boardEvalCache.ContainsKey(board.ZobristKey)) {
-            logCount(LogCountType.evalBoardSavedHasCount, 1);
-            logTime(LogTimeType.evalBoardTime, cTimer.MillisecondsElapsedThisTurn-timeEvalBoardStart);
-            return boardEvalCache[board.ZobristKey];
-        }
-
         int eval = 0;
-        if (board.IsInCheck()) eval += 3;
-        if (board.IsInCheckmate()) eval += 1000000;
-        if (board.IsInStalemate() || board.IsInsufficientMaterial() || board.IsRepeatedPosition()) eval -= 100000;
-        if (board.IsDraw() || board.IsFiftyMoveDraw()) eval -= 10;
+        if (boardEvalCache.ContainsKey(board.ZobristKey)) {
+            logCount(LogCountType.evalBoardCacheCount, 1);
+            eval = boardEvalCache[board.ZobristKey];
+        } else {
+            if (board.IsInCheck()) eval += 3;
+            if (board.IsInCheckmate()) eval += 1000000;
+            if (board.IsInStalemate() || board.IsInsufficientMaterial() || board.IsRepeatedPosition()) eval -= 100000;
+            if (board.IsDraw() || board.IsFiftyMoveDraw()) eval -= 10;
 
-        foreach (PieceList pieces in board.GetAllPieceLists()) {
-            foreach (Piece piece in pieces) {
-                eval += getPieceValue(board, piece) * (piece.IsWhite ^ !evaluatingForWhite ? 1 : -1);
+            foreach (PieceList pieces in board.GetAllPieceLists()) {
+                foreach (Piece piece in pieces) {
+                    eval += getPieceValue(board, piece) * (piece.IsWhite ^ !evaluatingForWhite ? 1 : -1);
+                }
             }
-        }
 
-        boardEvalCache.Add(board.ZobristKey, eval);
+            boardEvalCache[board.ZobristKey] = eval;
+        }
         logTime(LogTimeType.evalBoardTime, cTimer.MillisecondsElapsedThisTurn-timeEvalBoardStart);
         return eval;
     }
 
 
-    public int getPieceTypeValue(PieceType pieceType) {
-        return pieceType switch {
-            PieceType.Pawn => 1,
-            PieceType.Knight => 3,
-            PieceType.Bishop => 3,
-            PieceType.Rook => 5,
-            PieceType.Queen => 9,
-            PieceType.King => -100000,
-            _ => 0,
-        };
-    }
+    int[] pieceValue = new int[] {
+        0,1,3,3,5,9,-100000
+    };
+
+    Dictionary<Piece,int> pieceEvalCache = new Dictionary<Piece, int>();
 
     /// Gets the value of a piece based on its position on the board and other characteristics
     public int getPieceValue(Board board, Piece piece) {
         logCount(LogCountType.evalPieceCount, 1);
         int timeEvalPieceValueStart = cTimer.MillisecondsElapsedThisTurn;
-        int val = getPieceTypeValue(piece.PieceType);
+        int val;
+        if (pieceEvalCache.ContainsKey(piece)) {
+            logCount(LogCountType.evalPieceCacheCount, 1);
+            val = pieceEvalCache[piece];
+        } else {
+            val = pieceValue[(int) piece.PieceType];
+            pieceEvalCache[piece] = val;
+        }
 
         logTime(LogTimeType.evalPieceTime, cTimer.MillisecondsElapsedThisTurn-timeEvalPieceValueStart);
         return val;
@@ -432,11 +478,11 @@ public class MyBot : IChessBot
     /// Gets the value of a move  based on what it achieves
     public int scoreMove(Move move) {
         if (move.IsCapture)
-            return getPieceTypeValue(move.CapturePieceType);
+            return pieceValue[(int) move.CapturePieceType];
         if (move.IsEnPassant)
             return -1;
         if (move.IsPromotion)
-            return getPieceTypeValue(move.PromotionPieceType);
+            return pieceValue[(int) move.PromotionPieceType];
         if (move.IsCastles)
             return 1;
         
@@ -455,13 +501,14 @@ public class MyBot : IChessBot
     public enum LogCountType : int {
         ThinkCount = 0,
         minimaxCount = 1,
-        minimaxSavedCount = 2,
-        minimaxSavedDepthCount = 3,
+        minimaxCacheCount = 2,
+        minimaxDepthCacheCount = 3,
         getPossibleMovesCount = 4,
-        getPossibleMovesSavedHashCount = 5,
+        getPossibleMovesCacheCount = 5,
         evalBoardCount = 6,
-        evalBoardSavedHasCount = 7,
+        evalBoardCacheCount = 7,
         evalPieceCount = 8,
+        evalPieceCacheCount = 9,
     }
 
 
@@ -492,9 +539,9 @@ public class MyBot : IChessBot
                 sum += time;
             }
             if (timeLog[i].Count > 0)
-                Console.WriteLine("Average time for " + (LogTimeType) i + ": " + (sum/timeLog[i].Count));
+                Console.WriteLine("Time for " + (LogTimeType) i + ": " + (sum) + " (" + Math.Round(sum/ (float)timeLog[i].Count,2) + ")");
             else
-                Console.WriteLine("Average time for " + (LogTimeType) i + ": 0");
+                Console.WriteLine("Time for " + (LogTimeType) i + ": 0");
         }
 
         Console.WriteLine("Total moves: " + moveLog.Count);
