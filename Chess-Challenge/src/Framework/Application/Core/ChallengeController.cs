fba3247be13e17f5d8f0d5a72607f2a688bd15d4
Bill <billma.texas@gmail.com>
2023-07-31 21:05:46 -0500

    u

--- Chess-Challenge/src/Framework/Application/Core/ChallengeController.cs
+++ Chess-Challenge/src/Framework/Application/Core/ChallengeController.cs
@@ -43,8 +43,8 @@ namespace ChessChallenge.Application
 
         public static PlayerType player1Type = PlayerType.Human;
         public static PlayerType player2Type = PlayerType.Human;
-        public static PlayerType botToTest1 = PlayerType.MyBotV1_1;
-        public static PlayerType botToTest2 = PlayerType.MyBotV1_2;
+        public static PlayerType botToTest1 = PlayerType.MyBotV1_2;
+        public static PlayerType botToTest2 = PlayerType.MyBotV1_1;
 
         // Game state
         readonly Random rng;
